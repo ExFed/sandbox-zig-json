a49bffe0458dee6ce512a8cb57ba9ddf6d65554d
David Allen <david.allen@columnzero.com>
2025-02-22 12:08:08 -0500

    Move tokenizer functions into Tokenizer struct

--- src/parser.zig
+++ src/parser.zig
@@ -109,119 +109,119 @@ const Tokenizer = struct {
     source: []const u8,
 
     pub const Error = error{ UnclosedQuote, UnexpectedCharacter };
-};
 
-var err_location: Location = undefined;
-var err_lexeme: []const u8 = undefined;
+    var err_location: Location = undefined;
+    var err_lexeme: []const u8 = undefined;
 
-fn error_unclosed_quote(l: Location) Tokenizer.Error {
-    // std.debug.print("unclosed quote (@ {s})\n", .{l});
-    err_location = l;
-    return Tokenizer.Error.UnclosedQuote;
-}
+    fn error_unclosed_quote(l: Location) Tokenizer.Error {
+        // std.debug.print("unclosed quote (@ {s})\n", .{l});
+        err_location = l;
+        return Tokenizer.Error.UnclosedQuote;
+    }
 
-fn error_unexpected_char(c: []const u8, l: Location) Tokenizer.Error {
-    // std.debug.print("unexpected character: '{c}' (@ {s})\n", .{ c, l });
-    err_location = l;
-    err_lexeme = c;
-    return Tokenizer.Error.UnexpectedCharacter;
-}
+    fn error_unexpected_char(c: []const u8, l: Location) Tokenizer.Error {
+        // std.debug.print("unexpected character: '{c}' (@ {s})\n", .{ c, l });
+        err_location = l;
+        err_lexeme = c;
+        return Tokenizer.Error.UnexpectedCharacter;
+    }
 
-fn tokenize(allocator: Allocator, src: []const u8) (Allocator.Error || Tokenizer.Error)!std.ArrayList(Token) {
-    var tokens = std.ArrayList(Token).init(allocator);
-    errdefer tokens.deinit();
+    fn tokenize(allocator: Allocator, src: []const u8) (Allocator.Error || Tokenizer.Error)!std.ArrayList(Token) {
+        var tokens = std.ArrayList(Token).init(allocator);
+        errdefer tokens.deinit();
 
-    var i: usize = 0;
-    var loc: Location = Location.init();
-    while (i < src.len) : (i += 1) {
-        switch (src[i]) {
-            '\n' => loc.next_line(),
-            ' ', '\r', '\t' => loc.advance(),
-            '{' => {
-                try tokens.append(Token{ .token_type = TokenType.LBrace, .location = loc, .value = "{" });
-                loc.advance();
-            },
-            '}' => {
-                try tokens.append(Token{ .token_type = TokenType.RBrace, .location = loc, .value = "}" });
-                loc.advance();
-            },
-            '[' => {
-                try tokens.append(Token{ .token_type = TokenType.LBracket, .location = loc, .value = "[" });
-                loc.advance();
-            },
-            ']' => {
-                try tokens.append(Token{ .token_type = TokenType.RBracket, .location = loc, .value = "]" });
-                loc.advance();
-            },
-            ':' => {
-                try tokens.append(Token{ .token_type = TokenType.Colon, .location = loc, .value = ":" });
-                loc.advance();
-            },
-            ',' => {
-                try tokens.append(Token{ .token_type = TokenType.Comma, .location = loc, .value = "," });
-                loc.advance();
-            },
-            't' => {
-                if (i + 4 <= src.len and std.mem.eql(u8, src[i .. i + 4], "true")) {
-                    try tokens.append(Token{ .token_type = TokenType.True, .location = loc, .value = "true" });
-                    loc.advance_by(4);
-                    i += 3;
-                } else {
-                    return error_unexpected_char(src[i .. i + 1], loc);
-                }
-            },
-            'f' => {
-                if (i + 5 <= src.len and std.mem.eql(u8, src[i .. i + 5], "false")) {
-                    try tokens.append(Token{ .token_type = TokenType.False, .location = loc, .value = "false" });
-                    loc.advance_by(5);
-                    i += 4;
-                } else {
-                    return error_unexpected_char(src[i .. i + 1], loc);
-                }
-            },
-            'n' => {
-                if (i + 4 <= src.len and std.mem.eql(u8, src[i .. i + 4], "null")) {
-                    try tokens.append(Token{ .token_type = TokenType.Null, .location = loc, .value = "null" });
-                    loc.advance_by(4);
-                    i += 3;
-                } else {
-                    return error_unexpected_char(src[i .. i + 1], loc);
-                }
-            },
-            '"' => {
-                var j = i + 1;
-                while (j < src.len) : (j += 1) {
-                    if (src[j] == '"') {
-                        try tokens.append(Token{ .token_type = TokenType.String, .location = loc, .value = src[i + 1 .. j] });
-                        loc.advance_by(j - i + 1);
-                        i = j;
-                        break;
+        var i: usize = 0;
+        var loc: Location = Location.init();
+        while (i < src.len) : (i += 1) {
+            switch (src[i]) {
+                '\n' => loc.next_line(),
+                ' ', '\r', '\t' => loc.advance(),
+                '{' => {
+                    try tokens.append(Token{ .token_type = TokenType.LBrace, .location = loc, .value = "{" });
+                    loc.advance();
+                },
+                '}' => {
+                    try tokens.append(Token{ .token_type = TokenType.RBrace, .location = loc, .value = "}" });
+                    loc.advance();
+                },
+                '[' => {
+                    try tokens.append(Token{ .token_type = TokenType.LBracket, .location = loc, .value = "[" });
+                    loc.advance();
+                },
+                ']' => {
+                    try tokens.append(Token{ .token_type = TokenType.RBracket, .location = loc, .value = "]" });
+                    loc.advance();
+                },
+                ':' => {
+                    try tokens.append(Token{ .token_type = TokenType.Colon, .location = loc, .value = ":" });
+                    loc.advance();
+                },
+                ',' => {
+                    try tokens.append(Token{ .token_type = TokenType.Comma, .location = loc, .value = "," });
+                    loc.advance();
+                },
+                't' => {
+                    if (i + 4 <= src.len and std.mem.eql(u8, src[i .. i + 4], "true")) {
+                        try tokens.append(Token{ .token_type = TokenType.True, .location = loc, .value = "true" });
+                        loc.advance_by(4);
+                        i += 3;
+                    } else {
+                        return error_unexpected_char(src[i .. i + 1], loc);
                     }
-                }
-                if (j == src.len) {
-                    return error_unclosed_quote(loc);
-                }
-            },
-            else => {
-                if (src[i] >= '0' and src[i] <= '9') {
+                },
+                'f' => {
+                    if (i + 5 <= src.len and std.mem.eql(u8, src[i .. i + 5], "false")) {
+                        try tokens.append(Token{ .token_type = TokenType.False, .location = loc, .value = "false" });
+                        loc.advance_by(5);
+                        i += 4;
+                    } else {
+                        return error_unexpected_char(src[i .. i + 1], loc);
+                    }
+                },
+                'n' => {
+                    if (i + 4 <= src.len and std.mem.eql(u8, src[i .. i + 4], "null")) {
+                        try tokens.append(Token{ .token_type = TokenType.Null, .location = loc, .value = "null" });
+                        loc.advance_by(4);
+                        i += 3;
+                    } else {
+                        return error_unexpected_char(src[i .. i + 1], loc);
+                    }
+                },
+                '"' => {
                     var j = i + 1;
                     while (j < src.len) : (j += 1) {
-                        if ((src[j] < '0' or src[j] > '9') and src[j] != '.') {
-                            try tokens.append(Token{ .token_type = TokenType.Number, .location = loc, .value = src[i..j] });
-                            loc.advance_by(j - i);
-                            i = j - 1;
+                        if (src[j] == '"') {
+                            try tokens.append(Token{ .token_type = TokenType.String, .location = loc, .value = src[i + 1 .. j] });
+                            loc.advance_by(j - i + 1);
+                            i = j;
                             break;
                         }
                     }
-                } else {
-                    return error_unexpected_char(src[i .. i + 1], loc);
-                }
-            },
+                    if (j == src.len) {
+                        return error_unclosed_quote(loc);
+                    }
+                },
+                else => {
+                    if (src[i] >= '0' and src[i] <= '9') {
+                        var j = i + 1;
+                        while (j < src.len) : (j += 1) {
+                            if ((src[j] < '0' or src[j] > '9') and src[j] != '.') {
+                                try tokens.append(Token{ .token_type = TokenType.Number, .location = loc, .value = src[i..j] });
+                                loc.advance_by(j - i);
+                                i = j - 1;
+                                break;
+                            }
+                        }
+                    } else {
+                        return error_unexpected_char(src[i .. i + 1], loc);
+                    }
+                },
+            }
         }
-    }
 
-    return tokens;
-}
+        return tokens;
+    }
+};
 
 test "tokenize sample" {
     const src =
@@ -231,7 +231,7 @@ test "tokenize sample" {
     ;
 
     const allocator = std.heap.page_allocator;
-    const tokens = try tokenize(allocator, src);
+    const tokens = try Tokenizer.tokenize(allocator, src);
     defer allocator.free(tokens.items);
 
     const expected = [_]Token{
@@ -266,9 +266,9 @@ test "tokenize error: quote not closed" {
         \\  "quote...
         \\]
     ;
-    const result = tokenize(std.heap.page_allocator, src);
+    const result = Tokenizer.tokenize(std.heap.page_allocator, src);
     try std.testing.expectError(Tokenizer.Error.UnclosedQuote, result);
-    try std.testing.expectEqual(Location.of(3, 3), err_location);
+    try std.testing.expectEqual(Location.of(3, 3), Tokenizer.err_location);
 }
 
 test "tokenize error: unexpected character" {
@@ -278,8 +278,8 @@ test "tokenize error: unexpected character" {
         \\  "unexpected": !!!
         \\}
     ;
-    const result = tokenize(std.heap.page_allocator, src);
+    const result = Tokenizer.tokenize(std.heap.page_allocator, src);
     try std.testing.expectError(Tokenizer.Error.UnexpectedCharacter, result);
-    try std.testing.expectEqual(Location.of(3, 17), err_location);
-    try std.testing.expectEqualDeep("!", err_lexeme);
+    try std.testing.expectEqual(Location.of(3, 17), Tokenizer.err_location);
+    try std.testing.expectEqualDeep("!", Tokenizer.err_lexeme);
 }
